473453cc7387f46ee79f288378f44e57af6b0088
veramaerle-png <veramaerle@googlemail.com>
2025-10-27 19:23:06 +0100

    Create main.py
    1 code

---
+++ main.py
@@ -0,0 +1,5 @@
+my_words = ["Hi!", "I'm Alisa's mom", "Kodland"] 
+print(my_words[1])
+import random
+fakt = ["I'm a mom", "I'm 41", "I'm learning Phyton", "I'm russian", "I'm a woman"]
+print(random.choice(fakt))
