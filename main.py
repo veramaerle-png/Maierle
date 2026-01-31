6d21839dc20ba6f8c9de442b8cb1d691c8f52841
veramaerle-png <veramaerle@googlemail.com>
2026-01-31 22:10:49 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,44 @@
-my_words = ["Hi!", "I'm Alisa's mom", "Kodland"] 
-print(my_words[1])
-import random
-fakt = ["I'm a mom", "I'm 41", "I'm learning Phyton", "I'm russian", "I'm a woman"]
-print(random.choice(fakt))
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+def result_calculate(size, lights, device):
+    #Переменные для энергозатратности приборов
+    home_coef = 100
+    light_coef = 0.04
+    devices_coef = 5   
+    return size * home_coef + lights * light_coef + device * devices_coef 
+
+#Первая страница
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#Вторая страница
+@app.route('/<size>')
+def lights(size):
+    return render_template(
+                            'lights.html', 
+                            size=size
+                           )
+
+#Третья страница
+@app.route('/<size>/<lights>')
+def electronics(size, lights):
+    return render_template(
+                            'electronics.html',
+                            size = size, 
+                            lights = lights                           
+                           )
+
+#Расчет
+@app.route('/<size>/<lights>/<device>')
+def end(size, lights, device):
+    return render_template('end.html', 
+                            result=result_calculate(int(size),
+                                                    int(lights), 
+                                                    int(device)
+                                                    )
+                        )
+app.run(debug=True)
